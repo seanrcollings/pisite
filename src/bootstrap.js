a8b96ca6c6ba951bfa5343fe69b4ff840cb38b34
Sean Collings <seanrcollings@gmail.com>
2018-07-24 05:02:30 +0100

    Actually converted to a react app correctly this time

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,1 +1,15 @@
-// ES6 code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './style/main.scss';
+
+function main() {
+  ReactDOM.render(
+    <App />
+    , document.querySelector('.app-wrapper'));
+}
+
+document.addEventListener('DOMContentLoaded', main);
