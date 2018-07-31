23f38a1617798649f2233c543c063527c52604c8
Sean Collings <seanrcollings@gmail.com>
2018-07-31 23:06:19 +0100

    link boxes no operate correctly, but the text is cut off as they animate down

--- src/components/linkBox.js
+++ src/components/linkBox.js
@@ -31,28 +31,27 @@ export default class LinkBox extends Component {
         return (
 
             <div className = 'link-box' onMouseEnter = {() => this.toggleLink()} onMouseLeave = {() => this.toggleLink()}>
+                    <h2 className = 'link-box__title'>
+                        {this.props.title} 
+                    </h2>
+
+                    <div onClick = {() => this.toggleHeight()} className = {`link-box__icon ${this.state.height === 'auto' ? 'arrow-up' : ''}`}>
+                        <i className="fas fa-caret-down"></i>
+                    </div>
+
+                    <p className = 'link-box__subtitle' >
+                        {this.props.subTitle}
+                    </p>
+
+                    <a className = {`link-box__link ${this.state.linkStateClass}`}>
+                        Navigate To Page <i className="fas fa-caret-right"></i>
+                    </a>
                 
-                <h2 className = 'link-box__title'>
-                    {this.props.title} 
-                </h2>
-
-                <div onClick = {() => this.toggleHeight()} className = {`link-box__icon ${this.state.height === 'auto' ? 'arrow-up' : ''}`}>
-                    <i className="fas fa-caret-down"></i>
-                </div>
-
-                <p className = 'link-box__subtitle' >
-                    {this.props.subTitle}
-                </p>
-
                 <AnimateHeight duration = {300} height = {this.state.height}>
                     <div className = 'link-box__description'>
                         <p>{this.props.text}</p>
                     </div>
                 </AnimateHeight>
-
-                <a className = {`link-box__link ${this.state.linkStateClass}`}>
-                    Navigate To Page <i className="fas fa-caret-right"></i>
-                </a>
 
             </div>
         )
