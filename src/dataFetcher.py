d22cddd73d25e8ed3389598f70e450a4a2937044
Sean Collings <seanrcollings@gmail.com>
2018-08-08 15:54:57 -0600

    edited some of the commands so they run on the pi

--- src/dataFetcher.py
+++ src/dataFetcher.py
@@ -35,21 +35,21 @@ class DataType():
 		return hashlib.sha1(title.encode()).hexdigest()
 
 
-# data_types = [
-# 	DataType('Uptime', 'Time since last restart', 'uptime -p'),
-# 	DataType('Current Time', "You are a dummy if you do not know what current time it", "date +\%I:\%M\%p"), # displays the time in a human understandable format
-# 	DataType('Current Date', 'What the current date is one the pi internally', "date +\%d/\%m/\%Y"),
-# 	DataType('Load Average', 'Average Load of the systems', "uptime | grep -o '...., ...., ....'"), # command finds the particular format of: .02, 0.03, 0.01
-# 	DataType('User Count', 'How many users are currently logged into the system, will always be atleast 1', "uptime | grep -o '..users'"),
-#   DataType('Memory Usage', 'How much memory the Pi is using at the moment', "cat /proc/meminfo | egrep 'MemTotal|MemFree'", parseMemory),
-#   DataType('Disk Usage', 'How much space is currently taken up on the Pi', "df -h --total | grep total | grep '..%'"),
-#	DataType('Internal Temperature', 'The internal temperature of the Raspberry Pi', "/opt/vc/bin/vcgencmd measure_temp | grep .....C"),
-# 	DataType('Top Process PID', 'The PID of the process that is using the most CPU time', "ps -Ao pid --sort=-pcpu | head -n 2 | grep -Eo '[0-9]{1-4}'"),
-# 	DataType('Top Process Memory Usage', 'The memory usage of the process that is using the most CPU time', "ps -Ao pmem --sort=-pcpu | head -n 2 | grep -Eo '[0-9]{1-4}' | head -n 1")
-# ]
+data_types = [
+	DataType('Uptime', 'Time since last restart', 'uptime -p'),
+	DataType('Current Time', "You are a dummy if you do not know what current time it", "date +\%I:\%M\%p"), # displays the time in a human understandable format
+	DataType('Current Date', 'What the current date is one the pi internally', "date +\%d/\%m/\%Y"),
+	DataType('Load Average', 'Average Load of the systems', "uptime | grep -o '...., ...., ....'"), # command finds the particular format of: .02, 0.03, 0.01
+	DataType('User Count', 'How many users are currently logged into the system, will always be atleast 1', "uptime | grep -o '..users'"),
+	DataType('Memory Usage', 'How much memory the Pi is using at the moment', "cat /proc/meminfo | egrep 'MemTotal|MemFree'", parseMemory),
+	DataType('Disk Usage', 'How much space is currently taken up on the Pi', "df -h --total | grep total | grep -o '..%'"),
+	DataType('Internal Temperature', 'The internal temperature of the Raspberry Pi', "/opt/vc/bin/vcgencmd measure_temp | grep -o .....C"),
+	DataType('Top Process PID', 'The PID of the process that is using the most CPU time', "ps -Ao pid --sort=-pcpu | head -n 2 | tr -dc '0-9'"),
+	DataType('Process Memory', 'The memory usage of the process that is using the most CPU time', "ps -o pid --sort=-pmem | head -n 2 | tr -dc '0-9'") # fixed up some of these commands so they run on the pi
+]
 
-# for data_type in data_types:
-# 	data_type.fetch_data()
+for data_type in data_types:
+	data_type.fetch_data()
 
-# with open('/var/www/pisite/pisiteprod/data.json', 'w') as outfile:
-#     json.dump(data_objects, outfile)
+with open('/var/www/pisite/pisiteprod/data.json', 'w') as outfile:
+    json.dump(data_objects, outfile)
