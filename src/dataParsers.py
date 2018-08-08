fd1b4c8eff970851eab1ee3ffa45ea8bc5787655
Sean Collings <seanrcollings@gmail.com>
2018-08-08 15:58:56 -0600

    modified parsers output

--- src/dataParsers.py
+++ src/dataParsers.py
@@ -9,4 +9,4 @@ def parseMemory(command_output):
     mem_used = str(round(mem_used / 1000, 2))
     mem_total = str(round(mem_total / 1000, 2))
 
-    print(mem_percent + '% \n' + mem_used + 'MB / ' + mem_total + 'MB')
+    return mem_percent + '% \n' + mem_used + 'MB'
