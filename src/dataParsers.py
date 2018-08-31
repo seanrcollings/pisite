ea0fadc981b9e3f9ca41c5fbdaed880a3564a9b3
Sean Collings <seanrcollings@gmail.com>
2018-08-31 16:34:56 -0600

    Fixed error with reporting Memory usage percentage

--- src/dataParsers.py
+++ src/dataParsers.py
@@ -1,14 +1,12 @@
-import pdb
 
 def parseMemory(command_output):
     mem = [int(char) for char in command_output.split() if char.isdigit()]
 
     mem_total = mem[0]
-    mem_free = mem[1]
+    mem_free = float(mem[1])
     mem_used = mem_total - mem_free
 
-    mem_percent = str((mem_used/mem_total)*100)
+    mem_percent = str(round((mem_used/mem_total)*100))
     mem_total = str(round(mem_total / 1000, 2))
-    pbd.set_trace()
 
     return mem_percent + '% \n' + mem_total + 'MB'
