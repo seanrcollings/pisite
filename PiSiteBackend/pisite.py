773986dd3f4285a43d22eb8b25c5b066a01a2f34
Sean Collings <seanrcollings@gmail.com>
2019-02-23 12:58:14 -0700

    added back accidentially deleated file

---
+++ PiSiteBackend/pisite.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify, render_template, send_from_directory, request
+from flask_cors import CORS
+import json
+import sqlite3 
+import pdb
+
+from dataFetcher import fetch_data
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/stats/*": {"origins": "*"}})
+
+#ROUTES
+
+@app.route('/', defaults={'path':''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template('index.html')
+
+@app.route('/mastermind/index.html')
+def mastermind():
+    return render_template('mastermind/index.html')
+
+@app.route('/stats/data', methods=['GET', 'POST'])
+def data():
+    if request.method == 'GET':
+        stat_objects = {}
+        for stat in get_stats():
+            stat_objects[stat[5]] = {"id": stat[5], "description": stat[2], "title": stat[1], "data": fetch_data(stat[3])}
+        return json.dumps(stat_objects)
+
+@app.route('/static/<path:path>')
+def send_static_asset(path):
+    return send_from_directory('static', path)
+
+@app.route('/mastermind/static/<path:path>')
+def send_mastermind_static_asset(path):
+    return send_from_directory('templates/mastermind/static', path)
+
+
+
+# HELPER FUNCTIONS
+def add_stat(order, title, subtitle, command, parser, Id):
+    try:
+        with sqlite3.connect('pisite.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO stats (ord, title, subtitle, command, id) values (?, ?, ?, ?, ?);", (order, title, subtitle, command, Id))
+            result = {'status': 1, 'message': 'Stat Added'}
+    except:
+        result = {'status': 0, 'message': 'error, Stat not Added'}
+    return result
+
+def get_stats():
+    with sqlite3.connect('pisite.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM stats ORDER BY ord desc")
+        all_stats = cursor.fetchall()
+        return all_stats
+ 
+if __name__ == "__main__":
+    app.run()
+    
