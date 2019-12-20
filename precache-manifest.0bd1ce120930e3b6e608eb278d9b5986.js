4ed51263d5dfd37cd3b3e045eded8ee52ca010f3
Sean Collings <seanrcollings@gmail.com>
2019-12-20 15:18:49 -0700

    Updates

---
+++ precache-manifest.0bd1ce120930e3b6e608eb278d9b5986.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57e6a1d0802c0b284f1949057060e0c",
+    "url": "/pisite/index.html"
+  },
+  {
+    "revision": "b50923b5a86e60f85736",
+    "url": "/pisite/static/css/main.65765264.chunk.css"
+  },
+  {
+    "revision": "5c252384105d3268c3cf",
+    "url": "/pisite/static/js/2.98e90bb9.chunk.js"
+  },
+  {
+    "revision": "f6dd94a1fc15e9eee55428765727d170",
+    "url": "/pisite/static/js/2.98e90bb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b50923b5a86e60f85736",
+    "url": "/pisite/static/js/main.29d59640.chunk.js"
+  },
+  {
+    "revision": "fb9f427fd3ffc2c9c5a6",
+    "url": "/pisite/static/js/runtime-main.a4ea7cc8.js"
+  },
+  {
+    "revision": "6c34ae65e5d979709306ae8ae3180b16",
+    "url": "/pisite/static/media/SeanCollings.6c34ae65.png"
+  },
+  {
+    "revision": "58a917a0ef2cd60b2ea76fc6356132d8",
+    "url": "/pisite/static/media/a+.58a917a0.png"
+  },
+  {
+    "revision": "e9d36fc498f3b4d306bdef90e4c07a9c",
+    "url": "/pisite/static/media/apacheinstall.e9d36fc4.png"
+  },
+  {
+    "revision": "9807ddc90946bfcb18a34809eeed1e11",
+    "url": "/pisite/static/media/blue-mobo.9807ddc9.png"
+  },
+  {
+    "revision": "84330bbba5461c6d29d3da761db2dd29",
+    "url": "/pisite/static/media/blue.84330bbb.png"
+  },
+  {
+    "revision": "c79a11fc20b2af03ca45baee14f64574",
+    "url": "/pisite/static/media/cts.c79a11fc.png"
+  },
+  {
+    "revision": "59ee6a581ffe8b1aea99dc95d96d6a3e",
+    "url": "/pisite/static/media/logo-color.59ee6a58.png"
+  },
+  {
+    "revision": "031d59bd4ec9acfec57a76f98f19ed73",
+    "url": "/pisite/static/media/logo.031d59bd.png"
+  },
+  {
+    "revision": "e70070698bde7ca502590662f89b4de5",
+    "url": "/pisite/static/media/mta.e7007069.png"
+  },
+  {
+    "revision": "1ba77250316bc69cf3e69bece18258d3",
+    "url": "/pisite/static/media/netPro.1ba77250.png"
+  },
+  {
+    "revision": "b7f26e6dfe721bd955b39c1bd88c5985",
+    "url": "/pisite/static/media/npminstall.b7f26e6d.png"
+  },
+  {
+    "revision": "ca7f02cad363988296fae14b9cbf6782",
+    "url": "/pisite/static/media/npmstart.ca7f02ca.png"
+  },
+  {
+    "revision": "880705911303962834a94e8a2a9ad94c",
+    "url": "/pisite/static/media/pcPro.88070591.png"
+  },
+  {
+    "revision": "2b90afbe0f7377f92264a5182622d67b",
+    "url": "/pisite/static/media/ssh.2b90afbe.png"
+  }
+]);
